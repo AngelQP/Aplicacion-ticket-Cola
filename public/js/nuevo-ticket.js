7386e824ac2ee856b0fbe7360b4b028532829a3d
Angel Quispe <angelquispe_2997@outlook.com>
2023-05-30 11:11:59 -0500

    Proyecto avanzando hasta la emision de nuevo ticket

---
+++ public/js/nuevo-ticket.js
@@ -0,0 +1,31 @@
+// Referencias HTML
+const lblNuevoTicket = document.querySelector('#lblNuevoTicket')
+const btnCrear = document.querySelector('button')
+
+const socket = io();
+
+socket.on('connect', () => {
+  // console.log('Conectado');
+  btnCrear.disabled = false;
+});
+
+socket.on('disconnect', () => {
+    // console.log('Desconectado del servidor');
+  btnCrear.disabled = true;
+});
+
+socket.on('ultimo-ticket', (payload) => {
+  // console.log('Desconectado del servidor');
+  lblNuevoTicket.innerText = "Ticket " + payload;
+});
+
+btnCrear.addEventListener( 'click', () => {
+    
+    socket.emit( 'siguiente-ticket', null, ( ticket ) => {
+        console.log( 'Desde el servidor', ticket );
+        lblNuevoTicket.innerText = ticket;
+    });
+
+});
+
+console.log('Nuevo Ticket HTML');
